79ebd3fe45837bcf28b5cea2fdfeb4a215cb88f1
栗岡 陽平 <yohei.kurioka@thinkings.co.jp>
2022-07-13 16:53:18 +0900

    indexの編集(動いてはいない)

--- todo/todo-app/src/index.tsx
+++ todo/todo-app/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter } from "react-router-dom";
+import Login from './login/Login';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
